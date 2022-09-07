03e3c82e4f8d118f3a02f1fb0b408c96789e45b9
Dmitriy Maevskiy <blood_j@mail.ru>
2022-09-07 22:18:19 +0300

    Add split html

--- main.go
+++ main.go
@@ -25,6 +25,8 @@ func check(err error) {
 func main() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", index)
+	rtr.HandleFunc("/short", short)
+	rtr.HandleFunc("/status/{req}", status)
 	rtr.HandleFunc("/{req}", req)
 
 	http.Handle("/", rtr)
@@ -37,7 +39,7 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Printf("idx %s", r.URL)
 	// -- show start page
-	t, err := template.ParseFiles("template/index.html")
+	t, err := template.ParseFiles("template/index.html", "template/footer.html", "template/header.html")
 	check(err)
 	t.ExecuteTemplate(w, "index", nil)
 }
@@ -47,4 +49,18 @@ func req(w http.ResponseWriter, r *http.Request) {
 	log.Printf("req %s", r.URL)
 	// -- Request to Redis by URL and get long URL
 	// -- Redirect to url
+}
+
+func short(w http.ResponseWriter, r *http.Request) {
+	// -- Get url for shorting
+	// -- Generate short url
+	// -- Check new url in db for uniq
+	// -- Save data in db
+	// -- Redirect to status page
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	// -- Get url, separate short link
+	// -- Get info, from db
+	// -- Show status page
 }
